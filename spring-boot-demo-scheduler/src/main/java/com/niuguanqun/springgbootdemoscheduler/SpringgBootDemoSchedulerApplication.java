233184978b6c9273c64946953617b0c97d817191
niuguanqun <306565163@qq.com>
2020-12-11 14:16:28 +0800

    启动类开启定时任务

--- spring-boot-demo-scheduler/src/main/java/com/niuguanqun/springgbootdemoscheduler/SpringgBootDemoSchedulerApplication.java
+++ spring-boot-demo-scheduler/src/main/java/com/niuguanqun/springgbootdemoscheduler/SpringgBootDemoSchedulerApplication.java
@@ -1,9 +1,12 @@
 package com.niuguanqun.springgbootdemoscheduler;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling//开启定时
 public class SpringgBootDemoSchedulerApplication {
 
     public static void main(String[] args) {
