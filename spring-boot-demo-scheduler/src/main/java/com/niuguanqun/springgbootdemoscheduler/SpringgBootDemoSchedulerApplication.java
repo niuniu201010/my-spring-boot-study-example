a8f2317288d897a56c901a9bb02bd345f9ad07a0
niuguanqun <306565163@qq.com>
2020-12-11 11:25:35 +0800

    spring boot 定时器demo实例

---
+++ spring-boot-demo-scheduler/src/main/java/com/niuguanqun/springgbootdemoscheduler/SpringgBootDemoSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.niuguanqun.springgbootdemoscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringgBootDemoSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringgBootDemoSchedulerApplication.class, args);
+    }
+
+}
