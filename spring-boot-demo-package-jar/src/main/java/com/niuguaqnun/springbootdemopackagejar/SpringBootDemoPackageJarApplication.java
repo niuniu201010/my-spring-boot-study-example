edcc27f066d268cbe0126e2ad4d3401e37c8a2d8
niuguanqun <306565163@qq.com>
2020-12-18 16:42:37 +0800

    spring-boot jar打包+单元测试demo

---
+++ spring-boot-demo-package-jar/src/main/java/com/niuguaqnun/springbootdemopackagejar/SpringBootDemoPackageJarApplication.java
@@ -0,0 +1,13 @@
+package com.niuguaqnun.springbootdemopackagejar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoPackageJarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoPackageJarApplication.class, args);
+    }
+
+}
---
+++ spring-boot-demo-package-jar/src/test/java/com/niuguaqnun/springbootdemopackagejar/SpringBootDemoPackageJarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niuguaqnun.springbootdemopackagejar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoPackageJarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
