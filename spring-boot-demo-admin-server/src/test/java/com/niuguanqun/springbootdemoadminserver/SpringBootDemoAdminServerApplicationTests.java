fe9a8f31da3aebb0a5bf9a40b01c21b4d1c88add
niuguanqun <306565163@qq.com>
2020-12-24 14:54:39 +0800

    spring-boot admin监控服务端demo

---
+++ spring-boot-demo-admin-server/src/test/java/com/niuguanqun/springbootdemoadminserver/SpringBootDemoAdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niuguanqun.springbootdemoadminserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoAdminServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
