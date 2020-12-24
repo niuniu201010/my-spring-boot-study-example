938098829c77357111d6f0fdd2de16ee9f947e35
niuguanqun <306565163@qq.com>
2020-12-24 14:41:32 +0800

    spring-boot 文件上传demo

---
+++ spring-boot-demo-file-upload/src/test/java/com/niuguanqun/springbootdemofileupload/SpringBootDemoFileUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niuguanqun.springbootdemofileupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoFileUploadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
