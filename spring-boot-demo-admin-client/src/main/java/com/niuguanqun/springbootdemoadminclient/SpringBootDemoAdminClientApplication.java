a4ffc53be3c80c22cf4a243b55539219cf20e090
niuguanqun <306565163@qq.com>
2020-12-24 14:54:12 +0800

    spring-boot admin监控客户端demo

---
+++ spring-boot-demo-admin-client/src/main/java/com/niuguanqun/springbootdemoadminclient/SpringBootDemoAdminClientApplication.java
@@ -0,0 +1,13 @@
+package com.niuguanqun.springbootdemoadminclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoAdminClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoAdminClientApplication.class, args);
+    }
+
+}
