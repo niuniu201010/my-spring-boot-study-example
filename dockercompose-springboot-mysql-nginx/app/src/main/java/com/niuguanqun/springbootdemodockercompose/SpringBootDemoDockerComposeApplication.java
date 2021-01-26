28cdba45e9aff013b2aa96837f11ca10d1663432
niuguanqun <306565163@qq.com>
2021-01-26 14:21:20 +0800

    springboot 改造成docker-compose项目demo

---
+++ dockercompose-springboot-mysql-nginx/app/src/main/java/com/niuguanqun/springbootdemodockercompose/SpringBootDemoDockerComposeApplication.java
@@ -0,0 +1,13 @@
+package com.niuguanqun.springbootdemodockercompose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoDockerComposeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoDockerComposeApplication.class, args);
+    }
+
+}
---
+++ dockercompose-springboot-mysql-nginx/app/src/test/java/com/niuguanqun/springbootdemodockercompose/SpringBootDemoDockerComposeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niuguanqun.springbootdemodockercompose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoDockerComposeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
